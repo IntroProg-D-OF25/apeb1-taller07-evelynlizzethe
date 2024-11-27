b612965a7230bfca49bc64bcace71f60111969dd
evelynlizzethe <elriofrio1@utpl.edu.ec>
2024-11-26 22:08:33 -0500

    Taller7

--- Taller7/src/Problema4_JugadoresFutbol.java
+++ Taller7/src/Problema4_JugadoresFutbol.java
@@ -10,30 +10,75 @@ public class Problema4_JugadoresFutbol {
         double promedioEdades = 0, promedioEstaturas = 0, estatura, sumaEstaturas = 0;
         boolean bandera = true;
         do {
-            System.out.print("Ingrese el nombre, posición, edad y estatura "
-                    + "de cada jugador: ");
+            System.out.print("Ingrese el nombre del Jugador: ");
             nombreJugador = tcl.nextLine();
-            posicion = tcl.next();
+            System.out.print("Ingrese la posición en el campo: ");
+            posicion = tcl.nextLine();
+            System.out.print("Ingrese la edad del Jugador: ");
             edad = tcl.nextInt();
+            System.out.print("Ingrese la estatura del Jugador: ");
             estatura = tcl.nextDouble();
 
+            tcl.nextLine();
             sumaEdades += edad;
             sumaEstaturas += estatura;
-            contador++;
-            
-            System.out.printf("%s -%s-, edad %d, estatura %.2f\n",
-                    nombreJugador, posicion, edad, estatura);
-            System.out.println("Desea ingresar otro jugador? (s/n): ");
-            respuesta = tcl.next();
-            if (respuesta.equals("n")) {
+            contador = contador + 1;
+
+            System.out.printf(contador + ". " + nombreJugador + "\t-"
+                    + posicion + "-\t" + "edad\t " + edad + "\testatura\t"
+                    + estatura);
+
+            System.out.println("\nIngrese (r) para terminar de ingresar datos");
+            respuesta = tcl.nextLine();
+            if (respuesta.equals("r")) {
                 bandera = false;
             }
         } while (bandera);
-        promedioEdades = sumaEdades / numJugadores;
-        promedioEstaturas = sumaEstaturas / numJugadores;
-        
-        System.out.printf("Promedio de edades: %.1f\n", promedioEdades);
-        System.out.printf("Promedio de estaturas: %.2f\n", promedioEstaturas);
 
+        promedioEdades = sumaEdades / contador;
+        promedioEstaturas = sumaEstaturas / contador;
+
+        System.out.printf("\nPromedio de edades: %.1f\n", promedioEdades);
+        System.out.printf("\nPromedio de estaturas: %.2f\n", promedioEstaturas);
     }
 }
+
+/***
+ * Ingrese el nombre del Jugador: Alexander Dominguez
+ * Ingrese la posici�n en el campo: Arquero
+ * Ingrese la edad del Jugador: 32
+ * Ingrese la estatura del Jugador: 1,92
+ * 1. Alexander Dominguez	-Arquero-	edad	 32	estatura	1.92
+ * Ingrese (r) para terminar de ingresar datos
+ * 1
+ * Ingrese el nombre del Jugador: Xavier Arreaga
+ * Ingrese la posici�n en el campo: Defensa
+ * Ingrese la edad del Jugador: 24
+ * Ingrese la estatura del Jugador: 1,85
+ * 2. Xavier Arreaga	-Defensa-	edad	 24	estatura	1.85
+ * Ingrese (r) para terminar de ingresar datos
+ * 1
+ * Ingrese el nombre del Jugador: Moises Caicedo
+ * Ingrese la posici�n en el campo: Mediocentro
+ * Ingrese la edad del Jugador: 19
+ * Ingrese la estatura del Jugador: 1,88
+ * 3. Moises Caicedo	-Mediocentro-	edad	 19	estatura	1.88
+ * Ingrese (r) para terminar de ingresar datos
+ * 1
+ * Ingrese el nombre del Jugador: Angel Mena 
+ * Ingrese la posici�n en el campo: Delantero 
+ * Ingrese la edad del Jugador: 32
+ * Ingrese la estatura del Jugador: 1,75
+ * 4. Angel Mena 	-Delantero -	edad	 32	estatura	1.75
+ * Ingrese (r) para terminar de ingresar datos
+ * 1
+ * Ingrese el nombre del Jugador: Michael Estrada 
+ * Ingrese la posici�n en el campo: Delantero 
+ * Ingrese la edad del Jugador: 27
+ * Ingrese la estatura del Jugador: 1,93
+ * 5. Michael Estrada 	-Delantero -	edad	 27	estatura	1.93
+ * Ingrese (r) para terminar de ingresar datos
+ * r
+ * Promedio de edades: 26,0
+ * Promedio de estaturas: 1,87
+ */
